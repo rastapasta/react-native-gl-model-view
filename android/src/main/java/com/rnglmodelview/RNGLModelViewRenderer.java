272e32bafbcdcefd52aab3e2cb6c5e72e085a663
patricevignola <vignola.patrice@gmail.com>
2017-11-24 08:47:21 -0500

    Fix a crash when hot-reloading the app

--- android/src/main/java/com/rnglmodelview/RNGLModelViewRenderer.java
+++ android/src/main/java/com/rnglmodelview/RNGLModelViewRenderer.java
@@ -26,8 +26,6 @@ public class RNGLModelViewRenderer implements GLSurfaceView.Renderer {
   private boolean hasToCreateBuffer = false;
   private int w = 0;
   private int h = 0;
-  private int fps = 0;
-  private int lfps = 0;
 
   private long time = System.currentTimeMillis();
   private FrameBuffer fb;
@@ -39,7 +37,7 @@ public class RNGLModelViewRenderer implements GLSurfaceView.Renderer {
   private RGBColor clearColor = new RGBColor(255, 255, 255);
   private Context mContext;
   private static RNGLModelViewRenderer master = null;
-  private GL10 lastInstance = null;
+  private GL10 previousGL = null;
   private float scale = 0.05f;
 
   public RNGLModelViewRenderer(Context context) {
@@ -57,14 +55,14 @@ public class RNGLModelViewRenderer implements GLSurfaceView.Renderer {
   public void onSurfaceChanged(GL10 gl, int width, int height) {
     Resources res = mContext.getResources();
 
-    if (master == null) {
+    if (world == null) {
       fb = new FrameBuffer(width, height);
       world = new World();
 
       light = new Light(world);
       light.enable();
       light.setIntensity(51, 51, 51);
-      light.setPosition(SimpleVector.create(-0.5f, 1f, 0.5f));
+      light.setPosition(SimpleVector.create(0, 0, 1));
 
       // In jpct, the coordinate system is rotated 180 degrees around x compared to the OpenGL
       // coordinate system, which means that y faces towards the bottom and z faces away from the
@@ -74,12 +72,23 @@ public class RNGLModelViewRenderer implements GLSurfaceView.Renderer {
       cam.rotateX((float)Math.PI);
 
       if (mModel != null) {
-        if (mTexture != null) {
-          TextureManager tm = TextureManager.getInstance();
-          tm.addTexture("texture", mTexture);
+        TextureManager tm = TextureManager.getInstance();
+
+        if (mTexture == null) {
+          if (tm.containsTexture("texture")) {
+            tm.removeTexture("texture");
+          }
+        } else {
+          if (tm.containsTexture("texture")) {
+            tm.replaceTexture("texture", mTexture);
+          } else {
+            tm.addTexture("texture", mTexture);
+          }
+
           TextureInfo ti = new TextureInfo(TextureManager.getInstance().getTextureID("texture"));
           mModel.setTexture(ti);
         }
+
         mModel.setSpecularLighting(true);
         mModel.build();
         mModel.strip();
@@ -89,38 +98,19 @@ public class RNGLModelViewRenderer implements GLSurfaceView.Renderer {
       MemoryHelper.compact();
 
       world.compileAllObjects();
-
-      if (master == null) {
-        master = RNGLModelViewRenderer.this;
-      }
-    } else if (lastInstance != gl) {
-      this.hasToCreateBuffer = true;
-      w = width;
-      h = height;
+    } else if (previousGL != gl) {
+      fb = new FrameBuffer(width, height);
     }
 
-    lastInstance = gl;
+    previousGL = gl;
   }
 
   @Override
   public void onDrawFrame(GL10 gl) {
-    if (this.hasToCreateBuffer) {
-      hasToCreateBuffer = false;
-      fb = new FrameBuffer(w, h);
-    }
-
     fb.clear(clearColor);
     world.renderScene(fb);
     world.draw(fb);
     fb.display();
-
-    if (System.currentTimeMillis() - time >= 1000) {
-      lfps = fps;
-      fps = 0;
-      time = System.currentTimeMillis();
-    }
-
-    fps++;
   }
 
   public void setModel(Object3D model) {
