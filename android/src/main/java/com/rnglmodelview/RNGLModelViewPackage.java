0bd1fd1f003b57e0846c5ca166e01c91c4614b0f
PatriceVignola <vignola.patrice@gmail.com>
2018-03-23 23:17:26 +0900

    Upgrade the module and the example project to react-native 0.47
    Since react-native 0.47 introduces non-backwards compatible changes for the android modules API, we had to set the minimum supported react-native version to 0.47 to allow the module to be used with projects using the latest versions of react-native.

--- android/src/main/java/com/rnglmodelview/RNGLModelViewPackage.java
+++ android/src/main/java/com/rnglmodelview/RNGLModelViewPackage.java
@@ -7,23 +7,19 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class RNGLModelViewPackage implements ReactPackage {
 
   @Override
-  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-    return Collections.emptyList();
-  }
-
-  @Override
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
-  }
-
-  @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
     return Arrays.<ViewManager>asList(new RNGLModelViewManager());
+  }
+
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+    return new ArrayList<NativeModule>();
   }
 }
